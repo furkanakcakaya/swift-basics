0c9c1017c20d00043aec17f515492e343b6a67c9
furkan <furkan.akcakaya@ykteknoloji.com>
2022-08-11 17:42:39 +0300

    initial commit. Project created

---
+++ Design Basics/Design Basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design Basics
+//
+//  Created by Yapı Kredi Teknoloji A.Ş. on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Design Basics/Design BasicsUITests/Design_BasicsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Design_BasicsUITestsLaunchTests.swift
+//  Design BasicsUITests
+//
+//  Created by Yapı Kredi Teknoloji A.Ş. on 11.08.2022.
+//
+
+import XCTest
+
+class Design_BasicsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
