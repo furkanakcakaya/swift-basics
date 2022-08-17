e96c904f749980ef8e7c842d163b299ac405aca9
furkan <furkan.akcakaya@ykteknoloji.com>
2022-08-17 16:40:06 +0300

    Calculator design Stack, stack in stack Very helpful practice.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Yapı Kredi Teknoloji A.Ş. on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
